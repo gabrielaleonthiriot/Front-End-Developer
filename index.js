e145659b4d3340004f8c2f2ab201d96c1d960556
gabrielaleonthiriot <gabriela.leon993@gmail.com>
2022-08-19 16:42:28 -0500

    div class container

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 alert("Hello😁");
+
+let firstName = prompt("What is your first name");
+let lastName = prompt("What is your last name");
+let fullName = firstName + " " + lastName;
+alert(fullName);
